686cfa6016e0d190582420eff056e2a28538c61c
jeongle <jyjjeong@gmail.com>
2018-09-13 16:56:56 -0700

    basic hours component structure

--- client/src/components/Hours/Hours.jsx
+++ client/src/components/Hours/Hours.jsx
@@ -1,1 +1,9 @@
 import React from 'React';
+
+const Hours = (props) => (
+  <div>
+    Hours Go Here
+  </div>
+)
+
+export default Hours;
---
+++ client/src/components/Hours/HoursItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HoursItem = (props) => (
+  <div>
+    This is a day's Hours
+  </div>
+)
+
+export default HoursItem;
