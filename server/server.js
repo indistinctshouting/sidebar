9190fd7e3beda47f47a71b6fe740caea2ec93b8c
jeongle <jyjjeong@gmail.com>
2018-09-21 19:50:30 -0700

    added cors

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const db = require('../db/db.js');
 const port = process.env.PORT || 3003;
+
 
 let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use('/:id', express.static(__dirname + '/../client/dist'));
 
 app.get('/summary/id/:id', (req, res) => {
